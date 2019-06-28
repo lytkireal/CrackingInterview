53cde9d827a28552a30d70d14ae7fa227e245534
Artem Lytkin <a.lytkin@netris.ru>
2019-06-28 12:04:49 +0300

    The Begin of going into the matter of Hashtable

--- HashTable.playground/Contents.swift
+++ HashTable.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+var dictionary: [String: String] = [:]
+print(abs("name".hashValue) % 3)
+print(abs("hobbie".hashValue) % 3)
+
+dictionary["name"] = "Artem"
+dictionary["name"] = "Football"
+
+dictionary["name"]
+dictionary["name"]
+
+public struct HashTable<Key: Hashable, Value> {
+    private typealias Element = (key: Key, value: Value)
+    private typealias Bucket = [Element]
+    
+    private var buckets: [Bucket]
+    
+    private(set) public var count = 0
+    
+    public var isEmpty: Bool { return count == 0 }
+    
+    public init(capacity: Int) {
+        assert(capacity > 0)
+        buckets = Array<Bucket>(repeatElement([], count: capacity))
+    }
+}
+
+let table = HashTable<String, String>(capacity: 5)
